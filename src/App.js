7a9a1cee8c7136a9b40499f499710664d1703caa
Cauã Mendes <cauamgomes6@gmail.com>
2022-05-16 11:42:44 -0300

    alterando aqui tlgd e se nao gostou que bom pq nem eu gostei

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BORA COMEÇAR A APANHAR NEGROS E BRANCOS E ASIATICOS E MARSHMALOW
         </p>
         <a
           className="App-link"
